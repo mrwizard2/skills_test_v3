448feb1e43be6fc3ce606e159809794d9db9227f
Laurence Shaw <info@courtoflawmm.com>
2023-05-29 22:37:14 -0500

    Coalition Technologies Skills Test V3

---
+++ js/main.js
@@ -0,0 +1,127 @@
+$(function() {
+
+  var siteSticky = function() {
+		$(".js-sticky-header").sticky({topSpacing:0});
+	};
+	siteSticky();
+
+	var siteMenuClone = function() {
+
+		$('.js-clone-nav').each(function() {
+			var $this = $(this);
+			$this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
+		});
+
+
+		setTimeout(function() {
+			
+			var counter = 0;
+      $('.site-mobile-menu .has-children').each(function(){
+        var $this = $(this);
+        
+        $this.prepend('<span class="arrow-collapse collapsed">');
+
+        $this.find('.arrow-collapse').attr({
+          'data-toggle' : 'collapse',
+          'data-target' : '#collapseItem' + counter,
+        });
+
+        $this.find('> ul').attr({
+          'class' : 'collapse',
+          'id' : 'collapseItem' + counter,
+        });
+
+        counter++;
+
+      });
+
+    }, 1000);
+
+		$('body').on('click', '.arrow-collapse', function(e) {
+      var $this = $(this);
+      if ( $this.closest('li').find('.collapse').hasClass('show') ) {
+        $this.removeClass('active');
+      } else {
+        $this.addClass('active');
+      }
+      e.preventDefault();  
+      
+    });
+
+		$(window).resize(function() {
+			var $this = $(this),
+				w = $this.width();
+
+			if ( w > 768 ) {
+				if ( $('body').hasClass('offcanvas-menu') ) {
+					$('body').removeClass('offcanvas-menu');
+				}
+			}
+		})
+
+		$('body').on('click', '.js-menu-toggle', function(e) {
+			var $this = $(this);
+			e.preventDefault();
+
+			if ( $('body').hasClass('offcanvas-menu') ) {
+				$('body').removeClass('offcanvas-menu');
+				$this.removeClass('active');
+			} else {
+				$('body').addClass('offcanvas-menu');
+				$this.addClass('active');
+			}
+		}) 
+
+		// click outisde offcanvas
+		$(document).mouseup(function(e) {
+	    var container = $(".site-mobile-menu");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+	      if ( $('body').hasClass('offcanvas-menu') ) {
+					$('body').removeClass('offcanvas-menu');
+				}
+	    }
+		});
+	}; 
+	siteMenuClone();
+
+  $(document).ready(
+  	$("#mountain1").addClass("focus"),
+  	$(document).on("click","#mountain1", function(){
+  		$(".climb-navbar-container a").removeClass("focus");
+		  $(this).addClass("focus");
+		}),
+  	$(document).on("click","#mountain2", function(){
+  		$(".climb-navbar-container a").removeClass("focus");
+		  $(this).addClass("focus");
+		})
+  );
+
+  $(document).ready(function(){
+	  // Activate Carousel
+	  $("#myCarousel").carousel();
+	    
+	  // Enable Carousel Indicators
+	  $(".item1").click(function(){
+	    $("#myCarousel").carousel(0);
+	  });
+	  $(".item2").click(function(){
+	    $("#myCarousel").carousel(1);
+	  });
+	  $(".item3").click(function(){
+	    $("#myCarousel").carousel(2);
+	  });
+	  $(".item4").click(function(){
+	    $("#myCarousel").carousel(3);
+	  });
+	    
+	  // Enable Carousel Controls
+	  $(".left").click(function(){
+	    $("#myCarousel").carousel("prev");
+  		console.log("Document is ready!");
+	  });
+	  $(".right").click(function(){
+	    $("#myCarousel").carousel("next");
+	  });
+	});
+
+});
